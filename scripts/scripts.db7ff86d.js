fe6471add579af6c4e45967d307a3bf2cdb019ac
Eric C. Brown <djheir@gmail.com>
2014-10-24 09:54:44 -0500

    Adds $http call & markup to show ingredients.

--- scripts/scripts.db7ff86d.js
+++ scripts/scripts.db7ff86d.js
@@ -1,4 +1,12 @@
 "use strict";angular.module("client2App",["ngResource","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).when("/about",{templateUrl:"views/about.html",controller:"AboutCtrl"}).when("/admin/orders",{templateUrl:"views/admin/orders.html",controller:"OrdersCtrl",resolve:{orders:["Order",function(a){return a.index()}]}}).when("/order",{templateUrl:"views/order.html",controller:"OrderFormCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("client2App").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]),angular.module("client2App").controller("AboutCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]),angular.module("client2App").controller("OrdersCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]),angular.module("client2App").controller("OrderCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]),angular.module("client2App").service("Order",function(){})
-.controller('OrderFormCtrl', function($scope) {
+.controller('OrderFormCtrl', function($scope, $http) {
   $scope.foo = 'bar';
+  $scope.ingredients = [];
+  $scope.total = 0;
+  // Here is where you call your views url from drupal.
+  // If this app is not on the same domain as your Drupal install you will
+  // run into CORS issues.
+  $http.get('http://foo.bar').success(function(ingredients) {
+    $scope.ingredients = ingredients;
+  });
 });
